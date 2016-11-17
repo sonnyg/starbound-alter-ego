c5addce5bec11ef11bce98a84014ce2fe08309f3
sonnyg <sonny@evagabond.com>
2016-11-17 13:00:33 -0800

    created script to unpack assets

---
+++ src/unpack.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const os = require('os');
+
+const starbound_root = `${os.homedir()}/Library/Application Support/Steam/steamapps/common/Starbound`;
+const asset_unpacker = `${starbound_root}/osx/asset_unpacker`;
+const asset_root = `${starbound_root}/assets`;
+
+const asset_source = `${asset_root}/packed.pak`;
+const asset_target = `${asset_root}/_unpacked2`;
+
+console.log(`Unpacking Starbound assets: ${asset_root}`);
+
+const execFile = require('child_process').execFile;
+
+const child = execFile(asset_unpacker, [asset_source, asset_target], (error, stdout, stderr) => {
+  if (error) {
+    // console.error(error);
+    throw error;
+  }
+
+  console.log(stdout);
+});
+
+console.log(`Process: ${child.pid} spawned. Depending on your system, this may take some time...`);
