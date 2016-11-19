81eed56fcc8b58f72a7fcc139936c0012088c00e
sonnyg <sonny@evagabond.com>
2016-11-19 00:05:58 -0800

    added creation of extra dye colors

---
+++ src/dye-descriptors.js
@@ -0,0 +1,27 @@
+// list shades light to dark
+const dyeDescriptors = [
+      { color: "Light Red", shades: ["e86d46", "d1422e", "96201b"] },
+      { color: "Dark Red", shades: ["ad2716", "8f1f1b", "731a1a"] },
+      { color: "Rusty Red", shades: ["be471b", "9b2f0c", "702710"] },
+      { color: "Light Brown", shades: ["7f5a39", "5b3523", "3b1f15"] },
+      { color: "Dirty Orange", shades: ["a6671d", "915622", "743e1d"] },
+      { color: "Dirty Blonde", shades: ["eaa758", "cd8b3d", "93682c"] },
+      { color: "Blonde", shades: ["f1d992", "daba5e", "b59a4d"] },
+      { color: "Light Yellow", shades: ["f2eba3", "dcd28c", "c2b677"] },
+      { color: "Light Green", shades: ["a7d13d", "83ad22", "6b880b"] },
+      { color: "Dark Green", shades: ["3b7f44", "2e6a38", "1d4b28"] },
+      { color: "Turquoise", shades: ["5bd5b6", "4aa9ad", "237082"] },
+      { color: "Light Turquoise", shades: ["49e193", "3ba380", "197c81"] },
+      { color: "Light Blue", shades: ["61bcde", "5381cc", "2c489e"] },
+      { color: "Dark Blue", shades: ["425879", "343965", "242247"] },
+      { color: "Dark Purple", shades: ["7f3b6d", "6a2e53", "4b1d30"] },
+      { color: "Hot Pink", shades: ["cd72d9", "ac4da6", "913b86"] },
+      { color: "Light Purple", shades: ["ad68e2", "8d41b0", "6a2980"] },
+      { color: "Brown Grey", shades: ["50422f", "36261e", "170f0d"] },
+      { color: "Grey Blue", shades: ["6f6d85", "525269", "3e4352"] },
+      { color: "Light Grey", shades: ["b8b8b8", "828282", "555555"] }
+  ];
+
+module.exports = {
+  dyeDescriptors
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const dyeDescriptors = require('./dye-descriptors.js').dyeDescriptors
 
 function clearBuild() {
 
@@ -24,8 +25,41 @@ function writeMetadata(metadata) {
   console.log('.metadata file created');
 }
 
+function createDye(modName, index, descriptor) {
+  return {
+    itemName: `${modName}${descriptor.color.toLowerCase().replace(' ', '')}dye`,
+    price: 25,
+    rarity: "Common",
+    category: "clothingDye",
+    inventoryIcon: `/items/generic/dyes/bluedye.png?replace;5588d4=${descriptor.shades[0]};344495=${descriptor.shades[1]};1a1c51=${descriptor.shades[2]}`,
+    description: "This coloured dye can be applied to a piece of armour or clothing with a right-click.",
+    shortdescription: `${descriptor.color} Dye`,
+    dyeColorIndex: index,
+    radioMessagesOnPickup: [ "pickupdye" ],
+    scripts: [ "/scripts/augments/dye.lua" ]
+  }
+}
+
+function writeDyeFiles(modName, dyeDescriptors) {
+  const offset = 12;  // these are the preexisting colors
+  dyeDescriptors.forEach((dyeDescriptor, index) => {
+    const dye = createDye(modName, index + offset, dyeDescriptor);
+    const fileName = `./build/${modName}/items/generic/dyes/${dye.itemName}.augment`;
+
+    writeFile(fileName, dye);
+  })
+}
+
+function writeFile(fileName, data) {
+  // 'pretty' print the json
+  fs.writeFileSync(fileName, JSON.stringify(data, null, '\t'));
+
+  console.log(`file created: ${fileName}`);
+}
+
 const metadata = createMetadata();
 writeMetadata(metadata);
+writeDyeFiles(metadata.name, dyeDescriptors);
 
 // for (var key in process.env) {
   // console.log(`${key} : ${process.env[key]}`)
