1716ed80c7f278b60313f79cc66efcdc41aad2d2
sonnyg <sonny@evagabond.com>
2016-11-29 15:01:53 -0800

    wrap unpack and pack logic inside functions and export as module

--- src/pack.js
+++ src/pack.js
@@ -1,31 +1,39 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const config = require('./config.js')
+function packAssets() {
+  const fs = require('fs');
+  const config = require('./config.js')
 
-const mod_name = 'alter-ego';
-const pak_name = `${mod_name}.pak`;
+  const mod_name = 'alter-ego';
+  const pak_name = `${mod_name}.pak`;
 
-const pak_staging = 'build';
-const pak_source = `${pak_staging}/${mod_name}`;
-const pak_target = `${pak_staging}/${pak_name}`;
+  const pak_staging = 'build';
+  const pak_source = `${pak_staging}/${mod_name}`;
+  const pak_target = `${pak_staging}/${pak_name}`;
 
-const mod_target = `${config.modRoot}/${pak_name}`;
-const asset_packer = `${config.starboundRoot}/osx/asset_packer`;
+  const mod_target = `${config.modRoot}/${pak_name}`;
+  const asset_packer = `${config.starboundRoot}/osx/asset_packer`;
 
-console.log(`Packing mod: ${mod_name} => ${pak_name}`);
+  console.log(`Packing mod: ${mod_name} => ${pak_name}`);
 
-const execFile = require('child_process').execFile;
+  const execFile = require('child_process').execFile;
 
-const child = execFile(asset_packer, [pak_source, pak_target], (error, stdout, stderr) => {
-  if (error) {
-    console.error(error);
-    throw error;
-  }
+  const child = execFile(asset_packer, [pak_source, pak_target], (error, stdout, stderr) => {
+    if (error) {
+      console.error(error);
+      throw error;
+    }
 
-  console.log(stdout);
-  console.log(`deploying mod: ${mod_target}`);
+    console.log(stdout);
+    console.log(`deploying mod: ${mod_target}`);
 
-  // this may not seem like an efficient copy but it is OS independent
-  fs.createReadStream(pak_target).pipe(fs.createWriteStream(mod_target));
-});
+    // this may not seem like an efficient copy but it is OS independent
+    fs.createReadStream(pak_target).pipe(fs.createWriteStream(mod_target));
+  });
+}
+
+packAssets();
+
+module.exports = {
+  packAssets
+}
--- src/unpack.js
+++ src/unpack.js
@@ -1,29 +1,37 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const config = require('./config.js')
+function unpackAssets() {
+  const fs = require('fs');
+  const config = require('./config.js')
 
-const asset_unpacker = `${config.starboundRoot}/osx/asset_unpacker`;
-const asset_source = `${config.assetRoot}/packed.pak`;
-const asset_target = `${config.assetRoot}/_unpacked`;
+  const asset_unpacker = `${config.starboundRoot}/osx/asset_unpacker`;
+  const asset_source = `${config.assetRoot}/packed.pak`;
+  const asset_target = `${config.assetRoot}/_unpacked`;
 
-console.log(`Using asset location: ${config.assetRoot}`);
-console.log(`Unpacking assets to: ${asset_target}`);
+  console.log(`Using asset location: ${config.assetRoot}`);
+  console.log(`Unpacking assets to: ${asset_target}`);
 
-// skip unpacking if directory is present
-if (!fs.existsSync(asset_target)) {
-  const execFile = require('child_process').execFile;
+  // skip unpacking if directory is present
+  if (!fs.existsSync(asset_target)) {
+    const execFile = require('child_process').execFile;
 
-  const child = execFile(asset_unpacker, [asset_source, asset_target], (error, stdout, stderr) => {
-    if (error) {
-      // console.error(error);
-      throw error;
-    }
+    const child = execFile(asset_unpacker, [asset_source, asset_target], (error, stdout, stderr) => {
+      if (error) {
+        // console.error(error);
+        throw error;
+      }
 
-    console.log(stdout);
-  });
+      console.log(stdout);
+    });
 
-  console.log(`Process: ${child.pid} spawned. Depending on your system, this may take some time...`);
-} else {
-  console.log(`Assets already unpacked. Skipping unpack.`);
+    console.log(`Process: ${child.pid} spawned. Depending on your system, this may take some time...`);
+  } else {
+    console.log(`Assets already unpacked. Skipping unpack.`);
+  }
+}
+
+unpackAssets();
+
+module.exports = {
+  unpackAssets
 }
